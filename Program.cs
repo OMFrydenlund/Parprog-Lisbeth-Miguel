f007cdd31f722f164457bc16b18387e9861d827a
OMFrydenlund <migfrydenlund@gmail.com>
2023-12-13 13:38:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Parprog_Lisbeth_Miguel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Wizard.heiIntroductionPotter();
+    }
+}
---
+++ Wizard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parprog_Lisbeth_Miguel
+{
+    public class Wizard
+    {
+        public Wizard(string name, string house, string[] inventory)
+        {
+            Name = name;
+            House = house;
+            Inventory = inventory;
+        }
+
+        public string Name { get; set; }
+        public string House { get; set; }
+        public string[] Inventory { get; set; }
+
+        public override string ToString()
+        {
+            string heiInventory = string.Join(", ", Inventory);
+            return $"{Name} of {House} house, with the following items in their inventory: {heiInventory}";
+        }
+
+        public static void heiIntroductionPotter()
+        {
+            string[] harryInventory = { "Elder Wand Senior", "Potion of greater luck", "En ugle" };
+            Wizard harryPotter = new Wizard("Harry Potter", "Gryffindor", harryInventory);
+            Console.WriteLine(harryPotter);
+        }
+    }
+}
